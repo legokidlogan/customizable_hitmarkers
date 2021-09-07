355ed1549f69df2ed8cc9a5d951a387f6c2671b9
legokidlogan <legokidlogan@gmail.com>
2021-09-07 16:37:57 -0600

    Add ratelimits

--- lua/custom_hitmarkers/server/sv_hitmarkers.lua
+++ lua/custom_hitmarkers/server/sv_hitmarkers.lua
@@ -2,12 +2,28 @@ CustomHitmarkers = CustomHitmarkers or {}
 CustomHitmarkers.HitUsers = CustomHitmarkers.HitUsers or {}
 CustomHitmarkers.NPCHitUsers = CustomHitmarkers.NPCHitUsers or {}
 CustomHitmarkers.EntHitUsers = CustomHitmarkers.EntHitUsers or {}
+CustomHitmarkers.RatelimitCount = CustomHitmarkers.RatelimitCount or {}
+CustomHitmarkers.RatelimitBlocked = CustomHitmarkers.RatelimitBlocked or {}
 CustomHitmarkers.HEAD_DIST = 10
 CustomHitmarkers.HEAD_DIST_SQUARED = CustomHitmarkers.HEAD_DIST ^ 2
+
+local convarFlags = { FCVAR_ARCHIVE, FCVAR_PROTECTED }
+
+local RATELIMIT_ENABLED = CreateConVar( "custom_hitmarkers_ratelimit_enabled", 1, convarFlags, "Enables ratelimiting on hitmarkers to prevent net message spam.", 0, 1 )
+local RATELIMIT_TRACK_DURATION = CreateConVar( "custom_hitmarkers_ratelimit_track_duration", 0.5, convarFlags, "The window of time (in seconds) for hit counts to be tracked per player before getting reset to 0.", 0, 50000 )
+local RATELIMIT_THRESHOLD = CreateConVar( "custom_hitmarkers_ratelimit_threshold", 50, convarFlags, "How many hit events a player must trigger in short succesion to become ratelimited.", 2, 50000 )
+local RATELIMIT_COOLDOWN = CreateConVar( "custom_hitmarkers_ratelimit_cooldown", 2, convarFlags, "How long (in seconds) hit events for a player will be ignored after breaching the ratelimit threshold.", 0, 50000 )
 
 local hitUsers = CustomHitmarkers.HitUsers
 local npcHitUsers = CustomHitmarkers.NPCHitUsers
 local entHitUsers = CustomHitmarkers.EntHitUsers
+local ratelimitCount = CustomHitmarkers.RatelimitCount
+local ratelimitBlocked = CustomHitmarkers.RatelimitBlocked
+
+local ratelimitEnabled = RATELIMIT_ENABLED:GetBool()
+local ratelimitTrackDuration = RATELIMIT_TRACK_DURATION:GetFloat()
+local ratelimitThreshold = RATELIMIT_THRESHOLD:GetInt()
+local ratelimitCooldown = RATELIMIT_COOLDOWN:GetFloat()
 
 util.AddNetworkString( "CustomHitmarkers_Hit" )
 util.AddNetworkString( "CustomHitmarkers_Kill" )
@@ -15,12 +31,59 @@ util.AddNetworkString( "CustomHitmarkers_EnableChanged" )
 util.AddNetworkString( "CustomHitmarkers_NPCEnableChanged" )
 util.AddNetworkString( "CustomHitmarkers_EntEnableChanged" )
 
+cvars.AddChangeCallback( "custom_hitmarkers_ratelimit_enabled", function( _, old, new )
+    ratelimitEnabled = tobool( tonumber( new ) or tonumber( old ) )
+end )
+
+cvars.AddChangeCallback( "custom_hitmarkers_ratelimit_track_duration", function( _, old, new )
+    ratelimitTrackDuration = tonumber( new ) or tonumber( old ) or 0.5
+end )
+
+cvars.AddChangeCallback( "custom_hitmarkers_ratelimit_threshold", function( _, old, new )
+    ratelimitThreshold = math.floor( tonumber( new ) or tonumber( old ) or 50 )
+end )
+
+cvars.AddChangeCallback( "custom_hitmarkers_ratelimit_cooldown", function( _, old, new )
+    ratelimitCooldown = tonumber( new ) or tonumber( old ) or 2
+end )
+
+function CustomHitmarkers.RatelimitCheck( attacker )
+    if not ratelimitEnabled then return end
+    if ratelimitBlocked[attacker] then return true end
+
+    local hitCount = ( ratelimitCount[attacker] or 0 ) + 1
+    ratelimitCount[attacker] = hitCount
+
+    if hitCount == 1 then
+        timer.Create( "CustomHitmarkers_RatelimitClear_" .. attacker:SteamID(), ratelimitTrackDuration, 1, function()
+            if not IsValid( attacker ) then return end
+
+            ratelimitCount[attacker] = nil
+        end )
+    elseif hitCount >= ratelimitThreshold then
+        ratelimitBlocked[attacker] = true
+
+        timer.Remove( "CustomHitmarkers_RatelimitClear_" .. attacker:SteamID() )
+
+        timer.Simple( ratelimitCooldown, function()
+            if not IsValid( attacker ) then return end
+
+            ratelimitCount[attacker] = nil
+            ratelimitBlocked[attacker] = nil
+        end )
+
+        return true
+    end
+end
+
+local ratelimitCheck = CustomHitmarkers.RatelimitCheck
+
 hook.Add( "EntityTakeDamage", "CustomHitMarkers_TrackDamagePos", function( ent, dmg )
     if not IsValid( ent ) then return end
 
     local attacker = dmg:GetAttacker()
 
-    if ent == attacker or not hitUsers[attacker] then return end
+    if ent == attacker or not IsValid( attacker ) or not attacker:IsPlayer() or not hitUsers[attacker] then return end
 
     local isNPC = ent:IsNPC()
     local isPlayer = ent:IsPlayer()
@@ -30,6 +93,8 @@ hook.Add( "EntityTakeDamage", "CustomHitMarkers_TrackDamagePos", function( ent, 
     elseif not isPlayer and not entHitUsers[attacker] then
         return
     end
+
+    if ratelimitCheck( attacker ) then return end
 
     local damage = dmg:GetDamage()
     local pos = dmg:GetDamagePosition()
@@ -60,7 +125,8 @@ hook.Add( "EntityTakeDamage", "CustomHitMarkers_TrackDamagePos", function( ent, 
 end, HOOK_LOW )
 
 hook.Add( "PlayerHurt", "CustomHitmarkers_HitNotify", function( ply, attacker, newHealth, damage )
-    if not hitUsers[attacker] then return end
+    if not IsValid( attacker ) or not attacker:IsPlayer() or not hitUsers[attacker] then return end
+    if ratelimitCheck( attacker ) then return end
 
     attacker.hitmarkerPoints = attacker.hitmarkerPoints or {}
     local pos = attacker.hitmarkerPoints[ply]
